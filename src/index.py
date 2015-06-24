71f9c4dc422e50bb8fe012144f1e4da9b56e0506
Kim Kiogora <kimkiogora@gmail.com>
2015-06-24 09:47:20 +0300

    Fixed Redis storage bug - pickle

--- src/index.py
+++ src/index.py
@@ -6,6 +6,8 @@ from RedisLib import RCache
 import urllib2
 import sys
 import time
+import pickle
+import re
 
 app = Flask(__name__)
 
@@ -22,14 +24,57 @@ redis = RCache('localhost', 6379)
 def index():
     return "Usage: Flask Search Sample"
 
-"Find all related people a person may know"
 def suggested_friends(global_friend_list, related_friends):
+    """
+    Function to suggest friends ( people you may know )
+    :param global_friend_list:
+    :param related_friends:
+    :return:list
+    """
     relations = []
     for m in related_friends:
         if m in global_friend_list.keys():
-            relations.append(global_friend_list[m])
-
+            m_list = global_friend_list[m]
+            for item in m_list:
+                relations.append(item)
     return relations
+
+
+def process_data(name, person_list, global_friends_list):
+    """
+    re-usable function to find friend suggestions
+    :param name:
+    :param person_list:
+    :param global_friends_list:
+    :return:set
+    """
+    final_response = {}
+    for j, k in enumerate(person_list):
+        if k == name:
+            "identify people 'this' person may know"
+            persons_friends = global_friends_list[name]
+            global pymk
+            pymk = suggested_friends(global_friends_list, persons_friends)
+            resp = {
+                'person': '%s ' % name,
+                'status': 'success',
+                'people_(s)he_may_know': pymk,
+            }
+            final_response = resp
+            break
+
+    if len(response) <= 0:
+        "reverse search, that is from bottom up[ friends of friends ]" \
+            "get all the people, this persons friends are friends with"
+        other_p = []
+        for v in global_friends_list.keys():
+            if name in global_friends_list[v]:
+                sugg_list = global_friends_list[v]
+                if name in sugg_list: sugg_list.remove(name)
+                for item in sugg_list:
+                    other_p.append(item)
+        final_response = other_p
+    return final_response
 
 @app.route("/refunite/api/v1/search/<name>", methods=['GET'])
 def search(name):
@@ -37,30 +82,21 @@ def search(name):
     response = {}
     if name is None or len(name) == 0:
         data = {
-            'status': '500',
+            'status': 'failed',
             'message': 'Provide a name to search'
         }
         response = data
     else:
-        "Do stuff"
-        data = {
-            'status': 'success',
-            'person': name,
-            'message': 'Not found',
-            'friends': {},
-            'people_you_may_know': 'Not found',
-        }
         "Process data here"
         start_time = time.time()
-        user_data = redis.pop(name, 'people_list')
+        user_data = redis.pop('people_list')
         if user_data is None:
             "Data not in redis, may be the first time search is being done" \
                 "Iterate, local json url, http://localhost/people.json"
             try:
                 url_conn = urllib2.urlopen(data_url)
                 json_raw = url_conn.read()
-                json_object = json.loads(json_raw)
-                resp = json_object
+                resp = json.loads(json_raw)
 
                 "Work on the data here" \
                     "Filter, map-reduce then return appropriate response"
@@ -77,49 +113,39 @@ def search(name):
                     global_friends_list[person_is] = fl
 
                 "Push to redis for cache"
-                # redis.push('people_list', person_list)
-                # redis.push('people_friend_list', global_friends_list)
+                redis.push('people_list', pickle.dumps(person_list))
+                redis.push('people_friend_list', pickle.dumps(global_friends_list))
 
-                for j, k in enumerate(person_list):
-                    if k == name:
-                        "identify people 'this' person may know"
-                        persons_friends = global_friends_list[name]
-
-                        global pymk
-                        pymk = suggested_friends(global_friends_list, persons_friends)
-
-                        response = {
-                            'person': '%s ' % name,
-                            'status': 'success',
-                            #'friends': persons_friends,
-                            'people_(s)he_may_know': pymk,
-                        }
-                        break
-
-                if len(response) <= 0:
-                    "reverse search, that is from bottom up[ friends of friends ]"\
-                        "get all the people, this persons friends are friends with"
-                    other_p = []
-                    for v in global_friends_list.keys():
-                        if name in global_friends_list[v]:
-                            sugg_list = global_friends_list[v]
-                            if name in sugg_list: sugg_list.remove(name)
-                            other_p.append(sugg_list)
-
-                    response['person'] = '%s ' % name
-                    response['status'] = 'success'
-                    response['people_(s)he_may_know'] = other_p
+                p_resp = process_data(name,person_list, global_friends_list)
+                response['person'] = '%s ' % name
+                response['status'] = 'success'
+                response['people_(s)he_may_know'] = p_resp
             except:
                 response = {'error': '' % sys.exc_info()[0]}
         else:
-            response = user_data
+            global_friends_list = pickle.loads(redis.pop('people_friend_list'))
+            person_list = pickle.loads(user_data)
+            p_resp = process_data(name, person_list, global_friends_list)
+
+            did_you_mean = []
+            if len(p_resp) <= 0:
+                for i in person_list:
+                    if name.lower() in i.lower():
+                        if i not in did_you_mean:
+                            did_you_mean.append(i)
+                response['person'] = '%s ' % name
+                response['status'] = 'Nothing found'
+                response['did_you_mean'] = did_you_mean
+            else:
+                response['person'] = '%s ' % name
+                response['status'] = 'success'
+                response['people_(s)he_may_know'] = p_resp
         end = time.time()
         delta = end - start_time
 
         response['time_taken_to_respond'] = "%.2f sec" % delta
 
     return json.dumps(response)
-
 
 "Instantiate the application"
 if __name__ == "__main__":
